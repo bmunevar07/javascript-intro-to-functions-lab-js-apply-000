f72d51d2afe947943a8558fa66059752bfa9437f
Brian M <b29munevar@gmail.com>
2019-04-20 03:36:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase('HELLO!');
+}
+function whisper(string) {
+  return string.toLowerCase('Hello!');
+}
+function logShout(string) {
+  console.log(string.toUpperCase('HELLO!'));
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase('hello!'));
+}
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase());
+  console.log("I can't hear you!");
+}
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase());
+  return ("YES INDEED!");
+}
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.");
+  return ("I love you too.");
+}
